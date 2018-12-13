ec805cdfa12b08939d58f1755f7c295fa0d65881
OSCAR RIOS <oscr.rss@gmail.com>
2018-12-13 04:24:17 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -5,5 +5,9 @@ function lastImageSelector(){
   return $("img:last")
 }
 function ninjaBabySelector(){
+<<<<<<< HEAD
   return $("#baby-ninja")
+=======
+  return $("#ninja")
+>>>>>>> 0841cb816af57a757edb080b647821964eaf82aa
 }
